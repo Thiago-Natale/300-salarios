b876e5accf41191f1e26504e518b51610022d722
thiago-natale <thiagohenrique.natale@gmail.com>
2026-01-05 11:01:07 -0300

    Add api key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. CONFIGURAÇÃO INICIAL
 const SUPABASE_URL = "https://ddwlsisctmjqmhbajzmj.supabase.co";
-const SUPABASE_KEY = "SUA_CHAVE_AQUI"; // Certifique-se de usar a anon key correta
+const SUPABASE_KEY = "sb_publishable_X95ZSTtfoLiKm1oUPhXIUw_eSO4Kf6P"; // Certifique-se de usar a anon key correta
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const authContainer = document.getElementById('auth-container');
