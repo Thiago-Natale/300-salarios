d2f15261d83319c7e4ef4dfed58c767b809ecf66
thiago-natale <thiagohenrique.natale@gmail.com>
2026-01-06 15:49:51 -0300

    Export config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://ddwlsisctmjqmhbajzmj.supabase.co";
 const SUPABASE_KEY = "sb_publishable_X95ZSTtfoLiKm1oUPhXIUw_eSO4Kf6P"; // Certifique-se de usar a anon key correta
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
